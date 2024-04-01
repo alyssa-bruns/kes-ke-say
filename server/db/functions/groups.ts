e374995e7d3d3b489c3ff1040fb1a072709288ec
Matthew Nanthameechai <155590969+matthew-nanthameechai@users.noreply.github.com>
2024-04-02 11:29:13 +1300

    created db function to get all groups

--- server/db/functions/groups.ts
+++ server/db/functions/groups.ts
@@ -1,3 +1,8 @@
 import connection from '../connection'
+import { group } from '../../../models/group'
 
 const db = connection
+
+export async function getAllGroups(): Promise<group[]> {
+  return db('groups').select()
+}
