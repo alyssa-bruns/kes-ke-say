e2c5341fcbdbeba5fee05e53e1d00a0b01fb1ed2
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-04-03 09:54:27 +1300

    Add styling to footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -5,17 +5,17 @@ function Footer() {
     <>
       <footer className="mt-auto bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100">
         <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+          <div>
+            <img
+              src="/images/icons/icon-darkgray.png"
+              className="mr-5 h-6 sm:h-9"
+              alt="logo"
+            />
+            <p className="max-w-xs mt-4 text-sm text-gray-600">
+              Social media by Jean-Pierre.
+            </p>
+          </div>
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-            <div>
-              <img
-                src="/images/icons/icon-darkgray.png"
-                className="mr-5 h-6 sm:h-9"
-                alt="logo"
-              />
-              <p className="max-w-xs mt-4 text-sm text-gray-600">
-                Social media by Jean-Pierre.
-              </p>
-            </div>
             <div className="grid grid-cols-1 gap-8 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
               <div>
                 <p className="font-medium">Ze French News</p>
--- client/components/News.tsx
+++ client/components/News.tsx
@@ -2,23 +2,44 @@ import useNews from '../hooks/useNews.ts'
 import { Articles, News as NewsModel } from '../../models/news.ts'
 
 export default function News() {
-  const { data, isLoading, isError } = useNews()
+  // const { data, isLoading, isError } = useNews()
 
-  if (isLoading) {
-    return <p>wait</p>
-  }
+  // if (isLoading) {
+  //   return <p>wait</p>
+  // }
 
-  if (isError) {
-    return <p>Oops</p>
-  }
+  // if (isError) {
+  //   return <p>Oops</p>
+  // }
 
-  const newsData: Articles = data
-  const subsetNewsData = newsData.articles.slice(0, 5)
+  // const newsData: Articles = data
+  // const subsetNewsData = newsData.articles.slice(0, 5)
 
   return (
     <div>
       <h1>News</h1>
       <ul>
+        <li>
+          Grippe aviaire : première mondiale, une personne infectée par une
+          vache laitière - L'Indépendant
+        </li>
+        <li>
+          Attentat à Moscou : les individus arrêtés au Daguestan sont liés à
+          l'attaque - CNEWS
+        </li>
+        <li>
+          OM - PSG : Mbappé a refait des siennes à Marseille ? - Le10sport
+        </li>
+        <li>
+          OM-PSG: l'incompréhension de Dugarry après les explications de
+          l'arbitre sur ses décisions litigieuses - RMC Sport
+        </li>
+        <li>
+          Nanterre: le corps d'une femme, disparue depuis 2017, retrouvé dans le
+          coffre d'une voiture - BFM Paris Ile-de-France
+        </li>
+      </ul>
+      {/* <ul>
         {' '}
         {subsetNewsData.map((news: NewsModel) => (
           <li key={news.source.id}>
@@ -27,7 +48,7 @@ export default function News() {
             </a>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
