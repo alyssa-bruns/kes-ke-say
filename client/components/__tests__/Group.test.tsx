1b1fb6db3b27ff58d31e29fd1d3474b8a4854870
Matthew Nanthameechai <155590969+matthew-nanthameechai@users.noreply.github.com>
2024-04-03 11:04:50 +1300

    test for loading state completed

--- client/components/__tests__/Group.test.tsx
+++ client/components/__tests__/Group.test.tsx
@@ -1,37 +1,38 @@
 // @vitest-environment jsdom
-import { describe, it, expect, vi } from 'vitest'
+import '../../test-utils.tsx'
+import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
+import nock from 'nock'
+import { renderRoute } from '../../test-utils.tsx'
 
 import * as api from '../../apis/apiGroup.ts'
 
 import App from '../../components/App.tsx'
 import AllGroups from '../../components/AllGroups.tsx'
 
-import '../../test-utils.tsx'
-import { group } from '../../../models/group.ts'
-
-vi.mock('../../apis/apiGroup.ts')
-
-const fakeGroup = {
-  id: 1,
-  name: 'friendChips',
-  image: 'fries-darkgray.png',
-}
+const fakeGroups = [
+  { id: 1, name: 'friendChips', image: 'fries-darkgray.png' },
+  { id: 2, name: 'The fast and the curious', image: 'car-darkgray.png' },
+  { id: 3, name: 'Taco bout it', image: 'taco-darkgray.png' },
+]
 
 describe('allGroups', () => {
-  it('should render the group name and image', async () => {
+  it('should show loading state', async () => {
     //write test here
     // arrange
-    vi.mocked(api.getAllGroups()).mockImplementation(async () => {
-      return fakeGroup as group
-    })
+    const group = nock('http://localhost')
+      .get('/api/v1/groups')
+      .reply(200, fakeGroups)
 
     // act
-    render(<AllGroups />)
-    // assert
-    const loading = await screen.getByText('Loading...GroupPage')
+    renderRoute('/groups')
+    //  assert
+    const loading = await screen.findByText('Loading...GroupPage')
     expect(loading).toBeVisible()
+    // expect(group.isDone()).toBe(true)
   })
+
+  it.todo('show name and image', async () => {})
 })
 
 // describe('componentName', () =>{
