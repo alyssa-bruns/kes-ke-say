badf9684f489725adcb8cd5503fbda7a98e6fc54
Ben Atkin <jamin.ben.atkin@gmail.com>
2024-04-03 12:23:37 +1300

    Css and front end testing complete

--- client/components/UserProfilePage.tsx
+++ client/components/UserProfilePage.tsx
@@ -33,14 +33,34 @@ function UserProfilePage() {
 
   return (
     <>
-      <div key={profile.id}>
+      <br></br>
+      <div className="flex justify-end">
+        <button className="btn-blue">View All Posts by User</button>{' '}
+        <button className="btn-blue">View All Profiles</button>
+      </div>
+      <div className="profile-border" key={profile.id}>
         <img
+          className="profile-image"
           src={`/public/images/avatars/${profile.image}`}
           alt="profilePicture"
         />
-        <h4>Username: {profile.username}</h4>
-        <h3>Full Name: {profile.fullName}</h3>
-        <p>Location: {profile.location}</p>
+        <h4 className="profile-username">Username: {profile.username}</h4>
+        <h3 className="profile-fullName">Full Name: {profile.fullName}</h3>
+        <p className="profile-location">Location: {profile.location}</p>
+        <button className="btn-blue">Edit Profile</button>
+        <form>
+          <div>
+            <label>Bio </label>
+            <input
+              className="w-max h-max"
+              placeholder="Enter your bio..."
+            ></input>
+          </div>
+        </form>
+        <p>
+          <br />
+          <br />
+        </p>
       </div>
     </>
   )
---
+++ client/components/__tests__/UserProfilePage.test.tsx
@@ -0,0 +1,40 @@
+//@vitest-environment jsdom
+import {
+  describe,
+  it,
+  expect,
+  beforeEach,
+  vi,
+  afterAll,
+  beforeAll,
+} from 'vitest'
+import { renderRoute } from '../../test-utils'
+import nock from 'nock'
+
+beforeAll(() => {
+  nock.disableNetConnect()
+})
+
+const mockData = {
+  id: 3,
+  auth0Id: 'auth0|345',
+  username: 'shaq',
+  fullName: 'Shaquille Oatmeal',
+  location: 'Christchurch',
+  image: 'ava-16.png',
+}
+
+describe('<UserProfilePage />', () => {
+  it('Should show a users profile page', async () => {
+    // Arrange / Act
+    const scope = nock('http://localhost')
+      .get('/api/v1/users/profiles/shaq')
+      .reply(200, mockData)
+    const screen = renderRoute('/profiles/shaq')
+
+    const text = await screen.findByText(/Username: shaq/)
+    // Assert
+    expect(text).toBeVisible()
+    expect(scope.isDone()).toBe(true)
+  })
+})
