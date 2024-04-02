b7774dce7239ffe03f7076c33ae5bfbc85c2c653
Harrison Rogers <110720339+HarrisonRogers@users.noreply.github.com>
2024-04-03 11:10:24 +1300

    add instruction comments for Jean

--- client/components/News.tsx
+++ client/components/News.tsx
@@ -15,6 +15,7 @@ export default function News() {
   // const newsData: Articles = data
   // const subsetNewsData = newsData.articles.slice(0, 6)
 
+  // Comment the newsData mock data variable and uncomment the stuff above
   const newsData = [
     {
       id: 1,
@@ -63,6 +64,7 @@ export default function News() {
   ]
 
   return (
+    // Tailwind CSS liste card - posts - article list By maxacrea
     <div className="flex justify-center items-center h-screen bg-gray-50">
       <div className="flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 ">
         <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
@@ -72,48 +74,54 @@ export default function News() {
                 Jean Pierre dans Tresses collées
               </h2>
             </div>
-            <div className="grid w-full gap-10 grid-cols-3">
-              {newsData.map((news) => (
-                <div
-                  key={news.id}
-                  className="bg-white w-full rounded-lg shadow-md flex flex-col transition-all overflow-hidden hover:shadow-2xl"
-                >
-                  <div className="p-6">
-                    <div className="pb-3 mb-4 border-b border-stone-200 text-xs font-medium flex justify-between text-blue-900">
-                      <span className="flex items-center gap-1">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
+            <div
+              className="grid w-full gap-10 grid-cols-3"
+              style={{ maxHeight: 'calc(100vh - 300px)', overflowY: 'auto' }}
+            >
+              {newsData.map(
+                (
+                  news //subsetNewsData.map
+                ) => (
+                  <div
+                    key={news.id} // news.source.id
+                    className="bg-white w-full rounded-lg shadow-md flex flex-col transition-all overflow-hidden hover:shadow-2xl"
+                  >
+                    <div className="p-6">
+                      <div className="pb-3 mb-4 border-b border-stone-200 text-xs font-medium flex justify-between text-blue-900">
+                        <span className="flex items-center gap-1">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                            />
+                          </svg>
+                          {new Date(news.publishedAt).toLocaleDateString()}
+                        </span>
+                      </div>
+                      <h3 className="mb-4 font-semibold text-2xl">
+                        <a
+                          href={news.url}
+                          className="transition-all text-blue-900 hover:text-blue-600"
                         >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                          />
-                        </svg>
-                        {new Date(news.publishedAt).toLocaleDateString()}
-                      </span>
-                      <span>{/* Icon or additional info could go here */}</span>
+                          {news.title}
+                        </a>
+                      </h3>
+                      <p className="text-sky-800 text-sm mb-0">
+                        Article by {news.author}
+                      </p>
                     </div>
-                    <h3 className="mb-4 font-semibold text-2xl">
-                      <a
-                        href={news.url}
-                        className="transition-all text-blue-900 hover:text-blue-600"
-                      >
-                        {news.title}
-                      </a>
-                    </h3>
-                    <p className="text-sky-800 text-sm mb-0">
-                      Article by {news.author}
-                    </p>
+                    <div className="mt-auto"></div>
                   </div>
-                  <div className="mt-auto"></div>
-                </div>
-              ))}
+                )
+              )}
             </div>
           </div>
         </div>
