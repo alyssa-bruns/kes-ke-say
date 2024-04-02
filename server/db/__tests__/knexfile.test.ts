6f7becd3a064a61a02e34dd89261c3c872279f6e
Ben Atkin <jamin.ben.atkin@gmail.com>
2024-04-02 18:29:15 +1300

    Added tests for server side

--- server/db/__tests__/knexfile.test.ts
+++ server/db/__tests__/knexfile.test.ts
@@ -1,5 +1,16 @@
 import config from '../knexfile'
-import { describe, it, expect, vi } from 'vitest'
+import {
+  describe,
+  it,
+  expect,
+  vi,
+  beforeAll,
+  beforeEach,
+  afterAll,
+} from 'vitest'
+import connection from '../connection'
+import * as db from '../functions/users'
+import { User, UserSnakeCase } from '../../../models/user'
 
 describe('afterCreate PRAGMA', () => {
   it('enforces foreign keys (development)', () => {
@@ -8,4 +19,42 @@ describe('afterCreate PRAGMA', () => {
     config.development.pool.afterCreate(db, cb)
     expect(db.run).toHaveBeenCalledWith('PRAGMA foreign_keys = ON', cb)
   })
+})
+
+beforeAll(() => {
+  return connection.migrate.latest()
+})
+
+beforeEach(() => {
+  return connection.seed.run()
+})
+
+// TEMPLATE
+// describe('functionName', () => {
+//   it.todo('Explains what it should do', async () => {
+//     // Arrange
+//     // Act
+//     // Assert
+//   })
+// })
+
+describe('getProfileByUsername', () => {
+  it('It should display a profile from its username', async () => {
+    // Arrange / Act
+    const singleProfile = await db.getProfileByUsername('shaq')
+    const dummyData = {
+      id: 3,
+      auth0Id: 'auth0|345',
+      username: 'shaq',
+      fullName: 'Shaquille Oatmeal',
+      location: 'Christchurch',
+      image: 'ava-16.png',
+    }
+    // Assert
+    expect(singleProfile).toStrictEqual(dummyData)
+  })
+})
+
+afterAll(() => {
+  connection.destroy()
 })
