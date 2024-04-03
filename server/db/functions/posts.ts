6830f698359a4e92593fe36228f71037e77e1f3e
Joel Anderson <154105066+joel-anderson-24@users.noreply.github.com>
2024-04-03 14:24:55 +1300

    remove db function for single post

--- server/db/functions/posts.ts
+++ server/db/functions/posts.ts
@@ -16,18 +16,3 @@ export async function getAllPosts(): Promise<Post[]> {
     )
   return data
 }
-
-export async function getSinglePost(id: number) {
-  const data = await db('posts')
-    .join('users', 'users.id', 'posts.user_id')
-    .select(
-      'posts.id as postId',
-      'posts.body as body',
-      'posts.image as image',
-      'posts.created_at as createdAt',
-      'users.id as userId',
-      'users.username as username'
-    )
-    .where('postId', id)
-  return data
-}
