403cf86a9e07ba13f9cc206d9e4205d118747c42
Matthew Nanthameechai <155590969+matthew-nanthameechai@users.noreply.github.com>
2024-04-03 16:29:46 +1300

    added tailwind css to group page

--- client/components/AllGroups.tsx
+++ client/components/AllGroups.tsx
@@ -18,23 +18,64 @@ export function AllGroups() {
     return <h1>Error</h1>
   }
 
+  if (groupsData === undefined) {
+    return
+  }
+
   return (
     <>
       <h1>All groups</h1>
-      {groupsData.map((group) => (
-        <div key={group.id}>
-          <div>
-            <img
-              className="group-img"
-              src={`/images/icons/${group.image}`}
-              alt={group.name}
-            />
+      <div className="p-24 flex flex-wrap items-center justify-center">
+        {groupsData.map((group) => (
+          <div
+            key={group.id}
+            className="flex-shrink-0 m-6 relative overflow-hidden mt-auto bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-1000 rounded-lg max-w-xs shadow-lg"
+          >
+            <svg
+              className="absolute bottom-0 left-0 mb-8 "
+              viewBox="0 0 375 283"
+              fill="none"
+              // styleName="transform: scale(1.5); opacity: 0.1;"
+            >
+              <rect
+                x="159.52"
+                y="175"
+                width="152"
+                height="152"
+                rx="8"
+                transform="rotate(-45 159.52 175)"
+                fill="white"
+              />
+              <rect
+                y="107.48"
+                width="152"
+                height="152"
+                rx="8"
+                transform="rotate(-45 0 107.48)"
+                fill="white"
+              />
+            </svg>
+            <div className="relative pt-10 px-10 flex items-center justify-center">
+              <div className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"></div>
+              <img
+                className="relative w-40"
+                src={`/images/icons/${group.image}`}
+                alt={group.name}
+              />
+            </div>
+            <div className="relative text-black px-6 pb-6 mt-6">
+              <div className="flex justify-between">
+                <span className="block font-semibold text-xl">
+                  {group.name}
+                </span>
+                <span className="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
+                  Button
+                </span>
+              </div>
+            </div>
           </div>
-          <div>
-            <p>{group.name}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   )
 }
