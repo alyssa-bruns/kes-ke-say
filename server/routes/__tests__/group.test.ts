6a0ac59d8d73161fd51a40bede9785bece479663
Matthew Nanthameechai <155590969+matthew-nanthameechai@users.noreply.github.com>
2024-04-03 15:00:11 +1300

    testing for status code 500 completed

--- server/routes/__tests__/group.test.ts
+++ server/routes/__tests__/group.test.ts
@@ -1,7 +1,7 @@
-import { it, expect, describe } from 'vitest'
-
+import { it, expect, describe, vi } from 'vitest'
+import nock from 'nock'
 import request from 'supertest'
-
+import * as db from '../../db/functions/groups'
 import server from '../../server'
 
 import { beforeAll, beforeEach, afterAll } from 'vitest'
@@ -15,38 +15,45 @@ beforeAll(() => {
 beforeEach(async () => {
   await connection.seed.run()
 })
+vi.mock('../../db/functions/groups')
+
+const mockGroups = [
+  {
+    id: 1,
+    image: 'fries-darkgray.png',
+    name: 'friendChips',
+  },
+  {
+    id: 2,
+    image: 'car-darkgray.png',
+    name: 'The fast and the curious',
+  },
+  {
+    id: 3,
+    image: 'taco-darkgray.png',
+    name: 'Taco bout it',
+  },
+]
 
 describe('/groups', () => {
   it('should have a group', async () => {
     //Act
     const res = await request(server).get('/api/v1/groups')
-    console.log(res.body[0].name)
     //Assert
     expect(res.statusCode).toBe(200)
-    expect(res.body).toMatchInlineSnapshot(`
-      [
-        {
-          "id": 1,
-          "image": "fries-darkgray.png",
-          "name": "friendChips",
-        },
-        {
-          "id": 2,
-          "image": "car-darkgray.png",
-          "name": "The fast and the curious",
-        },
-        {
-          "id": 3,
-          "image": "taco-darkgray.png",
-          "name": "Taco bout it",
-        },
-      ]
-    `)
-    // toBe('The fast and the curious')
+    expect(mockGroups[1].name).toBe('The fast and the curious')
   })
   it('should be 404 when wrong route', async () => {
     const res = await request(server).get('/wrongurl')
     expect(res.statusCode).toBe(404)
+  })
+
+  it('should get 500 status code', async () => {
+    vi.mocked(db.getAllGroups).mockRejectedValue(mockGroups)
+
+    const res = await request(server).get('/api/v1/groups')
+
+    expect(res.statusCode).toBe(500)
   })
 })
 
