fa1ba606d5973692b045622ac7eed90abef0720d
Maitri Thakor <156578835+maitri-thakor@users.noreply.github.com>
2024-04-05 11:57:37 +1300

    Completed Css work for Group member list

--- client/components/GroupMemberList.tsx
+++ client/components/GroupMemberList.tsx
@@ -23,12 +23,35 @@ export function GroupMemberList() {
 
   return (
     <>
-      {memberData.map((member) => (
-        <div key={member.id}>
-          <img src={`/images/avatars/${member.image}`} alt={member.image} />
-          <p>{member.fullName}</p>
+      <div className="flex flex-col items-center justify-center min-h-screen p-16 bg-slate-200">
+        <h1 className="my-10 font-medium text-3xl sm:text-4xl font-black">
+          Members List
+        </h1>
+        <div className="mb-4"></div>
+        <div className="user-list w-full max-w-lg mx-auto bg-white rounded-xl shadow-xl flex flex-col py-4">
+          {memberData.map((member) => (
+            <div
+              key={member.id}
+              className="user-row flex flex-col items-center justify-between cursor-pointer  p-4 duration-300 sm:flex-row sm:py-4 sm:px-8 hover:bg-[#87c7ea]"
+            >
+              <div className="user flex items-center text-center flex-col sm:flex-row sm:text-left">
+                <div className="avatar-content mb-2.5 sm:mb-0 sm:mr-2.5">
+                  <img
+                    src={`/images/avatars/${member.image}`}
+                    alt={member.image}
+                  />
+                </div>
+                <div className="user-body flex flex-col mb-4 sm:mb-0 sm:mr-4">
+                  <a href="#" className="title font-medium no-underline">
+                    {member.fullName}
+                  </a>
+                  <div className="skills flex flex-col"></div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   )
 }
