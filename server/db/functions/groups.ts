3894f0c87034c4487791c677cb6a3ed78d6cfb24
Maitri Thakor <156578835+maitri-thakor@users.noreply.github.com>
2024-04-04 15:31:58 +1300

    Created getGroupMembersById Function & Get route

--- server/db/functions/groups.ts
+++ server/db/functions/groups.ts
@@ -12,3 +12,10 @@ export async function getGroupById(id: number): Promise<group[]> {
   const group = await db('groups').where({ id }).select().first()
   return group
 }
+
+export async function getGroupMembersById(user_id: number) {
+  const members = await db('group_members')
+    .where({ group_id: user_id })
+    .select()
+  return members
+}
--- server/routes/groups.ts
+++ server/routes/groups.ts
@@ -25,4 +25,15 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.get('/members/:user_id', async (req, res) => {
+  const user_id = Number(req.params.user_id)
+  try {
+    const members = await db.getGroupMembersById(user_id)
+    res.json(members)
+    res.status(200)
+  } catch (error) {
+    res.status(500).json([error])
+  }
+})
+
 export default router
